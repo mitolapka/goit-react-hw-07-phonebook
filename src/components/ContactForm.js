a0d8ab0b99796782dcb6e3804129ebd7aaec7391
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-10-19 13:28:29 +0300

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,76 +1,77 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from '../redux/contactsSlice';
-import { Label, Div, Button, DivName } from './MyForm.styled'
+import { Label, Div, Button, DivName } from './MyForm.styled';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { selectContacts } from '../redux/selectors';
 
 export const ContactForm = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
 
-    const validateName = value => {
-        let errorMessage;
-        if (!value) {
-            errorMessage = 'Required';
-        } else if (!/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/.test(value)) {
-            errorMessage = 'Name may contain only letters, apostrophe, dash and spaces.';
-        }
-        return errorMessage;
-    };
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const validateNumber = value => {
-        let errorMessage;
-        if (!value) {
-            errorMessage = 'Required';
-        } else if (!/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(value)) {
-            errorMessage = 'Invalid phone number format XXX-XX-XX';
-        }
-        return errorMessage;
-    };
+  const validateName = (value) => {
+    let errorMessage;
+    if (!value) {
+      errorMessage = 'Required';
+    } else if (!/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/.test(value)) {
+      errorMessage = 'Name may contain only letters, apostrophe, dash, and spaces.';
+    }
+    return errorMessage;
+  };
 
-    const handleFormSubmit = (values, { resetForm }) => {
-        const newContact = {
-          name: values.name,
-          number: values.number,
-        };
-        dispatch(addContact(newContact))
-          .then(() => {
-            resetForm();
-          });
+  const validateNumber = (value) => {
+    let errorMessage;
+    if (!value) {
+      errorMessage = 'Required';
+    } else if (!/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(value)) {
+      errorMessage = 'Invalid phone number format XXX-XX-XX';
+    }
+    return errorMessage;
+  };
+
+  const handleFormSubmit = (values, { resetForm }) => {
+    const { name, number } = values;
+
+    // Check for uniqueness
+    const isDuplicate = contacts.some(
+      (contact) => contact.name === name || contact.number === number
+    );
+
+    if (isDuplicate) {
+      setErrorMessage('Contact with the same name or number already exists.');
+      return;
     }
 
-    return (
-        
-        <Formik
-            initialValues={{ name: '', number: '' }}
-            onSubmit={handleFormSubmit}
-        >
-            <Form>
-                <DivName>
-                    <Label htmlFor="name">Name:</Label>
-                    <Field
-                        type="text"
-                        name="name"
-                        id="name"
-                        validate={validateName}
-                    />
-                    <ErrorMessage name="name" component="div" />
-                </DivName>
+    const newContact = {
+      name,
+      number,
+    };
+    dispatch(addContact(newContact)).then(() => {
+      resetForm();
+    });
+  };
 
-                <Div>
-                    <Label htmlFor="number">Phone Number:</Label>
-                    <Field
-                        type="tel"
-                        name="number"
-                        id="number"
-                        validate={validateNumber}
-                    />
-                    <ErrorMessage name="number" component="div" />
-                </Div>
+  return (
+    <Formik initialValues={{ name: '', number: '' }} onSubmit={handleFormSubmit}>
+      <Form>
+        <DivName>
+          <Label htmlFor="name">Name:</Label>
+          <Field type="text" name="name" id="name" validate={validateName} />
+          <ErrorMessage name="name" component="div" />
+        </DivName>
 
-                <Button type="submit">Add contact</Button>
-            </Form>
-        </Formik>
-    );
+        <Div>
+          <Label htmlFor="number">Phone Number:</Label>
+          <Field type="tel" name="number" id="number" validate={validateNumber} />
+          <ErrorMessage name="number" component="div" />
+        </Div>
+
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </Formik>
+  );
 };
-
-
